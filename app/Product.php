39c9748194e1ad55e2827cf59f2c4033570ae72b
Quan Ngo D. M <quan.ngo@asiantech.vn>
2020-03-13 18:24:27 +0700

    fix db

--- app/Product.php
+++ app/Product.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model {
-	protected $fillable = ['name', 'category_id', 'size', 'color', 'quantity', 'price', 'promotion_price', 'description'];
+	protected $fillable = ['name', 'category_id', 'size', 'color', 'quantity', 'price', 'promotion_price', 'description', 'is_active'];
 
 	public function category() {
 		return $this->belongsTo('App\Category');
