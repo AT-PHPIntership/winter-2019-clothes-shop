0d10c8ae316ffb4ffde188b3ccada889e6dbe711
Quan Ngo D. M <quan.ngo@asiantech.vn>
2020-03-16 21:36:00 +0700

    fix db

--- app/Image.php
+++ app/Image.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model {
 	protected $fillable = ['name', 'path', 'imageable_id', 'imageable_type'];
 
+	public function imageable() {
+		return $this->morphTo();
+	}
+
 }
