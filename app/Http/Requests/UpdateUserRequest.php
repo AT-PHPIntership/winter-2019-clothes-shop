95234b47d9ccf9aa1d4c5e5142c3c8b0d68d8d36
Tung Tran D <tung.tran@asiantech.vn>
2020-03-17 19:28:10 +0700

    crud user

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest {
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize() {
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules() {
+		return [
+			'name' => 'required|regex:/^[\pL\s\-]+$/u',
+			'email' => 'required|email|unique:users,email'
+			'address' => 'required',
+			'phone' => 'required|regex:/[0-9]{10}/',
+			'username' => 'required|unique:users,username'
+			'password' => 'required|min:6|max:20',
+			'role' => 'required',
+		];
+	}
+}
