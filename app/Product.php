62bea253ffb6fb41c83a7370363d0bf8c067bf0a
Tung Tran D <tung.tran@asiantech.vn>
2020-03-18 10:12:58 +0700

    crud product

---
+++ app/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+	protected $fillable = ['name', 'category_id', 'size', 'color', 'quantity', 'price', 'promotion_price', 'description', 'is_active'];
+
+	public function category() {
+		return $this->belongsTo('App\Category');
+	}
+
+	public function bills() {
+		return $this->belongsToMany('App\Bill');
+	}
+
+	public function comments() {
+		return $this->hasMany('App\Comment');
+	}
+
+	public function images() {
+		return $this->morphMany('App\Image', 'imageable');
+	}
+}
